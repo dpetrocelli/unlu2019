03b1dfa062a00b1e00f5ed83b1a45c22337d9851
David <dmeptrocelli@gmail.com>
2019-03-09 11:38:08 -0300

    First Station

---
+++ CH/src/main/java/Class1/Sockets/CliSocket.java
@@ -0,0 +1,10 @@
+package Class1.Sockets;
+
+public class CliSocket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CH/src/main/java/Class1/Sockets/ServerSocket.java
@@ -0,0 +1,10 @@
+package Class1.Sockets;
+
+public class ServerSocket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
